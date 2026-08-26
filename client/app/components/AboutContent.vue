d67edfe63b59e9b905b0b3fee132c4cf4efd7b3c
Don Wells <donwellsav@gmail.com>
2026-08-26 18:01:33 -0400

    chore: release 2.6.8

--- client/app/components/AboutContent.vue
+++ client/app/components/AboutContent.vue
@@ -100,7 +100,7 @@ const contributors = computed(() =>
 );
 
 // Get app version
-const appVersion = ref('2.6.7');
+const appVersion = ref('2.6.8');
 onMounted(async () => {
   if (import.meta.client && window.electronAPI?.getAppVersion) {
     appVersion.value = await window.electronAPI.getAppVersion();
